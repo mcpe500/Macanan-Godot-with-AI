555696fe0e9458fde2f3520d2dbdea1ea5dc2b95
mcpe500 <ivan04.android@gmail.com>
2024-12-06 15:49:33 +0700

    feat: Update generate_papan.gd to simplify node connection logic and adjust position scaling

--- src/generation/generate_papan.gd
+++ src/generation/generate_papan.gd
@@ -8,7 +8,9 @@ const Utils = preload("res://src/utils/utils.gd")
 func _ready() -> void:
 	var raw_positions = [
 		# Row 0
-		Vector2(0, 0), Vector2(2, 0), Vector2(3, 0), Vector2(4, 0), Vector2(5, 0), Vector2(6, 0), Vector2(8, 0),
+		#Vector2(0, 0), 
+		Vector2(2, 0), Vector2(3, 0), Vector2(4, 0), Vector2(5, 0), Vector2(6, 0), 
+		#Vector2(8, 0),
 		# Row 1
 		Vector2(0, 1), Vector2(1, 1), Vector2(2, 1), Vector2(3, 1), Vector2(4, 1), Vector2(5, 1), Vector2(6, 1), Vector2(7, 1), Vector2(8, 1),
 		# Row 2
@@ -16,13 +18,18 @@ func _ready() -> void:
 		# Row 3
 		Vector2(0, 3), Vector2(1, 3), Vector2(2, 3), Vector2(3, 3), Vector2(4, 3), Vector2(5, 3), Vector2(6, 3), Vector2(7, 3), Vector2(8, 3),
 		# Row 4
-		Vector2(0, 4), Vector2(2, 4), Vector2(3, 4), Vector2(4, 4), Vector2(5, 4), Vector2(6, 4), Vector2(8, 4)
+		#Vector2(0, 4), 
+		Vector2(2, 4), Vector2(3, 4), Vector2(4, 4), Vector2(5, 4), Vector2(6, 4), 
+		#Vector2(8, 4)
 	]
 	
 	# Create MapNode instances
 	var nodes: Array[MapNode] = []
 	for i in range(raw_positions.size()):
-		var scaled_pos = raw_positions[i] * BoardConstants.SPACING
+		var pos = raw_positions[i]
+		var scaled_x = pos.x * BoardConstants.SPACING
+		var scaled_y = (((1.1 * BoardConstants.SPACING) + BoardConstants.SPACING * pos.y * 0.5 if pos.x == 1 or pos.x == 7 else BoardConstants.SPACING * pos.y * 1))
+		var scaled_pos = Vector2(scaled_x, scaled_y)
 		nodes.append(MapNode.new(scaled_pos, i))
 	
 	# Connect nodes to the 8 surrounding nodes
@@ -43,40 +50,7 @@ func _ready() -> void:
 			var neighbor_pos = current_pos + direction
 			var neighbor_index = Utils.find_index(nodes, neighbor_pos)
 			if neighbor_index != -1:
-				# Exclude specific connections
-				if not (
-					(current_node.id == 2 and nodes[neighbor_index].id == 8) or
-					(current_node.id == 8 and nodes[neighbor_index].id == 2) or
-					(current_node.id == 9 and nodes[neighbor_index].id == 8) or
-					(current_node.id == 8 and nodes[neighbor_index].id == 9) or
-					(current_node.id == 9 and nodes[neighbor_index].id == 7) or
-					(current_node.id == 7 and nodes[neighbor_index].id == 9) or
-					(current_node.id == 7 and nodes[neighbor_index].id == 27) or
-					(current_node.id == 27 and nodes[neighbor_index].id == 7) or
-					(current_node.id == 26 and nodes[neighbor_index].id == 27) or
-					(current_node.id == 27 and nodes[neighbor_index].id == 26) or
-					(current_node.id == 26 and nodes[neighbor_index].id == 35) or
-					(current_node.id == 35 and nodes[neighbor_index].id == 26) or
-					(current_node.id == 1 and nodes[neighbor_index].id == 8) or
-					(current_node.id == 8 and nodes[neighbor_index].id == 1) or
-					(current_node.id == 9 and nodes[neighbor_index].id == 17) or
-					(current_node.id == 17 and nodes[neighbor_index].id == 9) or
-					(current_node.id == 17 and nodes[neighbor_index].id == 27) or
-					(current_node.id == 27 and nodes[neighbor_index].id == 17) or
-					(current_node.id == 5 and nodes[neighbor_index].id == 14) or
-					(current_node.id == 14 and nodes[neighbor_index].id == 5) or
-					(current_node.id == 13 and nodes[neighbor_index].id == 14) or
-					(current_node.id == 14 and nodes[neighbor_index].id == 13) or
-					(current_node.id == 23 and nodes[neighbor_index].id == 13) or
-					(current_node.id == 13 and nodes[neighbor_index].id == 23) or
-					(current_node.id == 23 and nodes[neighbor_index].id == 31) or
-					(current_node.id == 31 and nodes[neighbor_index].id == 23) or
-					(current_node.id == 32 and nodes[neighbor_index].id == 31) or
-					(current_node.id == 31 and nodes[neighbor_index].id == 32) or
-					(current_node.id == 39 and nodes[neighbor_index].id == 32) or
-					(current_node.id == 32 and nodes[neighbor_index].id == 39)
-				):
-					current_node.connected.append(nodes[neighbor_index])
+				current_node.connected.append(nodes[neighbor_index])
 
 	# Get the papan_place node
 	var papan_place = $papan_place
