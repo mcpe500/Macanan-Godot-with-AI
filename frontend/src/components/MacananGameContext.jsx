72f9ec25194c200618f652c2d96f05bd45714cf0
mcpe500 <ivan04.android@gmail.com>
2025-01-23 21:34:50 +0700

    feat: very unnecessary optimization

--- frontend/src/components/MacananGameContext.jsx
+++ frontend/src/components/MacananGameContext.jsx
@@ -22,282 +22,280 @@ export const MacananGameProvider = ({children}) => {
   const [firstMove, setFirstMove] = useState({uwong: true, macan: false});
   const location = useLocation(); 
 
-
-
   const connections = {
-    0: [1, 5, 6],
-    1: [0, 2, 6],
-    2: [1, 3, 6, 7, 8],
-    3: [2, 4, 8],
-    4: [3, 8, 9],
-    5: [0, 6, 10],
-    6: [0, 1, 2, 5, 7, 10, 11, 12],
-    7: [2, 6, 8, 12],
-    8: [2, 3, 4, 7, 9, 12, 13, 14],
-    9: [4, 8, 14],
-    10: [5, 6, 11, 15, 16, 26, 28, 30],
-    11: [6, 10, 12, 16],
-    12: [6, 7, 8, 11, 13, 16, 17, 18],
-    13: [8, 12, 14, 18],
-    14: [8, 9, 13, 18, 19, 31, 33, 35],
-    15: [10, 16, 20],
-    16: [10, 11, 12, 15, 17, 20, 21, 22],
-    17: [12, 16, 18, 22],
-    18: [12, 13, 14, 17, 19, 22, 23, 24],
-    19: [14, 18, 24],
-    20: [15, 16, 21],
-    21: [16, 20, 22],
-    22: [16, 17, 18, 21, 23],
-    23: [18, 22, 24],
-    24: [18, 19, 23],
-    25: [26, 27],
-    26: [25, 28, 10],
-    27: [25, 28, 29],
-    28: [26, 27, 10, 30],
-    29: [27, 30],
-    30: [28, 29, 10],
-    31: [14, 32, 33],
-    32: [31, 34],
-    33: [14, 31, 34, 35],
-    34: [32, 33, 36],
-    35: [14, 33, 36],
-    36: [34, 35]
+    0: new Uint8Array([1, 5, 6]),
+    1: new Uint8Array([0, 2, 6]),
+    2: new Uint8Array([1, 3, 6, 7, 8]),
+    3: new Uint8Array([2, 4, 8]),
+    4: new Uint8Array([3, 8, 9]),
+    5: new Uint8Array([0, 6, 10]),
+    6: new Uint8Array([0, 1, 2, 5, 7, 10, 11, 12]),
+    7: new Uint8Array([2, 6, 8, 12]),
+    8: new Uint8Array([2, 3, 4, 7, 9, 12, 13, 14]),
+    9: new Uint8Array([4, 8, 14]),
+    10: new Uint8Array([5, 6, 11, 15, 16, 26, 28, 30]),
+    11: new Uint8Array([6, 10, 12, 16]),
+    12: new Uint8Array([6, 7, 8, 11, 13, 16, 17, 18]),
+    13: new Uint8Array([8, 12, 14, 18]),
+    14: new Uint8Array([8, 9, 13, 18, 19, 31, 33, 35]),
+    15: new Uint8Array([10, 16, 20]),
+    16: new Uint8Array([10, 11, 12, 15, 17, 20, 21, 22]),
+    17: new Uint8Array([12, 16, 18, 22]),
+    18: new Uint8Array([12, 13, 14, 17, 19, 22, 23, 24]),
+    19: new Uint8Array([14, 18, 24]),
+    20: new Uint8Array([15, 16, 21]),
+    21: new Uint8Array([16, 20, 22]),
+    22: new Uint8Array([16, 17, 18, 21, 23]),
+    23: new Uint8Array([18, 22, 24]),
+    24: new Uint8Array([18, 19, 23]),
+    25: new Uint8Array([26, 27]),
+    26: new Uint8Array([25, 28, 10]),
+    27: new Uint8Array([25, 28, 29]),
+    28: new Uint8Array([26, 27, 10, 30]),
+    29: new Uint8Array([27, 30]),
+    30: new Uint8Array([28, 29, 10]),
+    31: new Uint8Array([14, 32, 33]),
+    32: new Uint8Array([31, 34]),
+    33: new Uint8Array([14, 31, 34, 35]),
+    34: new Uint8Array([32, 33, 36]),
+    35: new Uint8Array([14, 33, 36]),
+    36: new Uint8Array([34, 35])
   };
 
   const macanJump = {
     0: {
-      2: [1],
-      4: [1, 2, 3],
-      12: [6],
-      24: [6, 12, 18],
-      10: [5],
-      20: [5, 10, 15]
+      2: new Uint8Array([1]),
+      4: new Uint8Array([1, 2, 3]),
+      12: new Uint8Array([6]),
+      24: new Uint8Array([6, 12, 18]),
+      10: new Uint8Array([5]),
+      20: new Uint8Array([5, 10, 15])
     },
     1: {
-      3: [2],
-      11: [6],
-      21: [6, 11, 16]
+      3: new Uint8Array([2]),
+      11: new Uint8Array([6]),
+      21: new Uint8Array([6, 11, 16])
     },
     2: {
-      4: [3],
-      14: [8],
-      36: [8, 14, 35],
-      12: [7],
-      22: [7, 12, 17],
-      10: [6],
-      29: [6, 10, 30],
-      0: [1]
+      4: new Uint8Array([3]),
+      14: new Uint8Array([8]),
+      36: new Uint8Array([8, 14, 35]),
+      12: new Uint8Array([7]),
+      22: new Uint8Array([7, 12, 17]),
+      10: new Uint8Array([6]),
+      29: new Uint8Array([6, 10, 30]),
+      0: new Uint8Array([1])
     },
     3: {
-      13: [8],
-      23: [8, 13, 18],
-      1: [2]
+      13: new Uint8Array([8]),
+      23: new Uint8Array([8, 13, 18]),
+      1: new Uint8Array([2])
     },
     4: {
-      14: [9],
-      24: [9, 14, 19],
-      12: [8],
-      20: [8, 12, 16],
-      2: [3],
-      0: [3, 2, 1]
+      14: new Uint8Array([9]),
+      24: new Uint8Array([9, 14, 19]),
+      12: new Uint8Array([8]),
+      20: new Uint8Array([8, 12, 16]),
+      2: new Uint8Array([3]),
+      0: new Uint8Array([3, 2, 1])
     },
     5: {
-      7: [6],
-      9: [6, 7, 8],
-      15: [10]
+      7: new Uint8Array([6]),
+      9: new Uint8Array([6, 7, 8]),
+      15: new Uint8Array([10])
     },
     6: {
-      8: [7],
-      18: [12],
-      16: [11],
-      30: [10]
+      8: new Uint8Array([7]),
+      18: new Uint8Array([12]),
+      16: new Uint8Array([11]),
+      30: new Uint8Array([10])
     },
     7: {
-      9: [8],
-      17: [12],
-      5: [6]
+      9: new Uint8Array([8]),
+      17: new Uint8Array([12]),
+      5: new Uint8Array([6])
     },
     8: {
-      35: [14],
-      18: [13],
-      16: [12],
-      6: [7]
+      35: new Uint8Array([14]),
+      18: new Uint8Array([13]),
+      16: new Uint8Array([12]),
+      6: new Uint8Array([7])
     },
     9: {
-      19: [14],
-      7: [8],
-      5: [8, 7, 6]
+      19: new Uint8Array([14]),
+      7: new Uint8Array([8]),
+      5: new Uint8Array([8, 7, 6])
     },
     10: {
-      12: [11],
-      14: [11, 12, 13],
-      34: [11, 12, 13, 14, 33],
-      22: [16],
-      20: [15],
-      29: [30],
-      27: [28],
-      25: [26],
-      0: [5],
-      2: [6]
+      12: new Uint8Array([11]),
+      14: new Uint8Array([11, 12, 13]),
+      34: new Uint8Array([11, 12, 13, 14, 33]),
+      22: new Uint8Array([16]),
+      20: new Uint8Array([15]),
+      29: new Uint8Array([30]),
+      27: new Uint8Array([28]),
+      25: new Uint8Array([26]),
+      0: new Uint8Array([5]),
+      2: new Uint8Array([6])
     },
     11: {
-      13: [12],
-      33: [12, 13, 14],
-      21: [16],
-      28: [10],
-      1: [6]
+      13: new Uint8Array([12]),
+      33: new Uint8Array([12, 13, 14]),
+      21: new Uint8Array([16]),
+      28: new Uint8Array([10]),
+      1: new Uint8Array([6])
     },
     12: {
-      14: [13],
-      34: [13, 14, 33],
-      24: [18],
-      22: [17],
-      20: [16],
-      10: [11],
-      27: [11, 10, 28],
-      0: [6],
-      2: [7],
-      4: [8]
+      14: new Uint8Array([13]),
+      34: new Uint8Array([13, 14, 33]),
+      24: new Uint8Array([18]),
+      22: new Uint8Array([17]),
+      20: new Uint8Array([16]),
+      10: new Uint8Array([11]),
+      27: new Uint8Array([11, 10, 28]),
+      0: new Uint8Array([6]),
+      2: new Uint8Array([7]),
+      4: new Uint8Array([8])
     },
     13: {
-      33: [14],
-      23: [18],
-      11: [12],
-      28: [12, 11, 10],
-      3: [8]
+      33: new Uint8Array([14]),
+      23: new Uint8Array([18]),
+      11: new Uint8Array([12]),
+      28: new Uint8Array([12, 11, 10]),
+      3: new Uint8Array([8])
     },
     14: {
-      34: [33],
-      36: [35],
-      24: [19],
-      22: [18],
-      12: [13],
-      10: [13, 12, 11],
-      27: [13, 12, 11, 10, 28],
-      2: [8],
-      4: [9],
-      32: [31]
+      34: new Uint8Array([33]),
+      36: new Uint8Array([35]),
+      24: new Uint8Array([19]),
+      22: new Uint8Array([18]),
+      12: new Uint8Array([13]),
+      10: new Uint8Array([13, 12, 11]),
+      27: new Uint8Array([13, 12, 11, 10, 28]),
+      2: new Uint8Array([8]),
+      4: new Uint8Array([9]),
+      32: new Uint8Array([31])
     },
     15: {
-      17: [16],
-      19: [16, 17, 18],
-      5: [10]
+      17: new Uint8Array([16]),
+      19: new Uint8Array([16, 17, 18]),
+      5: new Uint8Array([10])
     },
     16: {
-      18: [17],
-      26: [10],
-      6: [11],
-      8: [12]
+      18: new Uint8Array([17]),
+      26: new Uint8Array([10]),
+      6: new Uint8Array([11]),
+      8: new Uint8Array([12])
     },
     17: {
-      19: [18],
-      15: [16],
-      7: [12]
+      19: new Uint8Array([18]),
+      15: new Uint8Array([16]),
+      7: new Uint8Array([12])
     },
     18: {
-      16: [17],
-      6: [12],
-      8: [13],
-      31: [14]
+      16: new Uint8Array([17]),
+      6: new Uint8Array([12]),
+      8: new Uint8Array([13]),
+      31: new Uint8Array([14])
     },
     19: {
-      17: [18],
-      15: [18, 17, 16],
-      9: [14]
+      17: new Uint8Array([18]),
+      15: new Uint8Array([18, 17, 16]),
+      9: new Uint8Array([14])
     },
     20: {
-      22: [21],
-      24: [21, 22, 23],
-      10: [15],
-      0: [15, 10, 5],
-      12: [16],
-      4: [16, 12, 8]
+      22: new Uint8Array([21]),
+      24: new Uint8Array([21, 22, 23]),
+      10: new Uint8Array([15]),
+      0: new Uint8Array([15, 10, 5]),
+      12: new Uint8Array([16]),
+      4: new Uint8Array([16, 12, 8])
     },
     21: {
-      23: [22],
-      11: [16],
-      1: [16, 11, 6]
+      23: new Uint8Array([22]),
+      11: new Uint8Array([16]),
+      1: new Uint8Array([16, 11, 6])
     },
     22: {
-      24: [23],
-      20: [21],
-      10: [16],
-      25: [16, 10, 26],
-      12: [17],
-      2: [17, 12, 7],
-      14: [18],
-      32: [18, 14, 31]
+      24: new Uint8Array([23]),
+      20: new Uint8Array([21]),
+      10: new Uint8Array([16]),
+      25: new Uint8Array([16, 10, 26]),
+      12: new Uint8Array([17]),
+      2: new Uint8Array([17, 12, 7]),
+      14: new Uint8Array([18]),
+      32: new Uint8Array([18, 14, 31])
     },
     23: {
-      21: [22],
-      13: [18],
-      3: [18, 13, 8]
+      21: new Uint8Array([22]),
+      13: new Uint8Array([18]),
+      3: new Uint8Array([18, 13, 8])
     },
     24: {
-      22: [23],
-      20: [23, 22, 21],
-      12: [18],
-      0: [18, 12, 6],
-      14: [19],
-      4: [19, 14, 9]
+      22: new Uint8Array([23]),
+      20: new Uint8Array([23, 22, 21]),
+      12: new Uint8Array([18]),
+      0: new Uint8Array([18, 12, 6]),
+      14: new Uint8Array([19]),
+      4: new Uint8Array([19, 14, 9])
     },
     25: {
-      10: [26],
-      22: [26, 10, 16],
-      29: [27]
+      10: new Uint8Array([26]),
+      22: new Uint8Array([26, 10, 16]),
+      29: new Uint8Array([27])
     },
     26: {
-      16: [10],
-      30: [28]
+      16: new Uint8Array([10]),
+      30: new Uint8Array([28])
     },
     27: {
-      10: [28],
-      12: [28, 10, 11],
-      14: [28, 10, 11, 12, 13],
-      34: [28, 10, 11, 12, 13, 14, 33]
+      10: new Uint8Array([28]),
+      12: new Uint8Array([28, 10, 11]),
+      14: new Uint8Array([28, 10, 11, 12, 13]),
+      34: new Uint8Array([28, 10, 11, 12, 13, 14, 33])
     },
     28: {
-      11: [10],
-      13: [10, 11, 12],
-      33: [10, 11, 12, 13, 14]
+      11: new Uint8Array([10]),
+      13: new Uint8Array([10, 11, 12]),
+      33: new Uint8Array([10, 11, 12, 13, 14])
     },
     29: {
-      25: [27],
-      10: [30],
-      2: [30, 10, 6]
+      25: new Uint8Array([27]),
+      10: new Uint8Array([30]),
+      2: new Uint8Array([30, 10, 6])
     },
     30: {
-      26: [28],
-      6: [10]
+      26: new Uint8Array([28]),
+      6: new Uint8Array([10])
     },
     31: {
-      35: [33],
-      18: [14]
+      35: new Uint8Array([33]),
+      18: new Uint8Array([14])
     },
     32: {
-      36: [34],
-      14: [31],
-      22: [31, 14, 18]
+      36: new Uint8Array([34]),
+      14: new Uint8Array([31]),
+      22: new Uint8Array([31, 14, 18])
     },
     33: {
-      13: [14],
-      11: [14, 13, 12],
-      28: [14, 13, 12, 11, 10]
+      13: new Uint8Array([14]),
+      11: new Uint8Array([14, 13, 12]),
+      28: new Uint8Array([14, 13, 12, 11, 10])
     },
     34: {
-      14: [33],
-      12: [33, 14, 13],
-      10: [33, 14, 13, 12, 11],
-      27: [33, 14, 13, 12, 11, 10, 28]
+      14: new Uint8Array([33]),
+      12: new Uint8Array([33, 14, 13]),
+      10: new Uint8Array([33, 14, 13, 12, 11]),
+      27: new Uint8Array([33, 14, 13, 12, 11, 10, 28])
     },
     35: {
-      8: [14],
-      31: [33]
+      8: new Uint8Array([14]),
+      31: new Uint8Array([33])
     },
     36: {
-      14: [35],
-      2: [35, 14, 8],
-      32: [34]
+      14: new Uint8Array([35]),
+      2: new Uint8Array([35, 14, 8]),
+      32: new Uint8Array([34])
     }
   };
 
